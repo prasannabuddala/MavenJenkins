cf34a8ce451ef7a2da8dda2aebef0e825ecbe0ea
prasa <prasa@LAPTOP-H0PSVFQQ>
2022-11-13 22:02:45 +0530

    first maven, git and jenkins commit

---
+++ src/main/java/mavenjenkins/maven_jenkins/App.java
@@ -0,0 +1,14 @@
+package mavenjenkins.maven_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Welcome to jenkins:)");
+    }
+}
